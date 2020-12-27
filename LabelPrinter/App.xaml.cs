39666325c98b1d1ef0f57277a6e7db50462ef431
fotianmoyin <190045431@qq.com>
2020-12-27 17:56:29 +0800

    1、添加全局日志捕获。

--- LabelPrinter/App.xaml.cs
+++ LabelPrinter/App.xaml.cs
@@ -13,5 +13,25 @@ namespace LabelPrinter
     /// </summary>
     public partial class App : Application
     {
+        NLog.Logger _log = NLog.LogManager.GetCurrentClassLogger();
+
+        public App()
+        {
+            App.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        void Current_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            _log.Error(e.Exception);
+            var comException = e.Exception as System.Runtime.InteropServices.COMException;
+            if (comException != null && comException.ErrorCode == -2147221040)//OpenClipboard HRESULT:0x800401D0(CLIPBRD_E_CANT_OPEN))
+                e.Handled = true;
+        }
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            _log.Error(e.ExceptionObject as Exception);
+        }
     }
 }
